77b30d1b39b0b5849ee937c444fee8098de4f348
IoanLT <ioan.tranole@gmail.com>
2020-11-19 10:26:15 +0000

    Testing methods with this keyword

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    state = {
+        name: 'Ryu',
+        age: 35
+    };
+
+    handleClick = () => {
+        console.log('The button has been clicked!');
+    }
+
+    handleMouseOver = () => {
+        this.setState({
+            name: 'Nelu'
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello there!</h1>
+                <p>Hi, my name is {this.state.name} and I am {this.state.age} years old.</p>  
+                <button onClick={this.handleClick}>Click me</button>  
+                <button onMouseOver={this.handleMouseOver}>Hover</button>  
+            </div>
+        );
+    }
+
+
+    
 }
 
 export default App;
